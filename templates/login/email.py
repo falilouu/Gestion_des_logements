b236de88b0a0a8e388965098b7f07e0db331de86
Falilou DIENG <40122223+falilouu@users.noreply.github.com>
2020-02-25 22:47:50 +0000

    Add files via upload

---
+++ templates/login/email.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+app =Flask(__name__)
+mail=Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'seckabdou632@gmail.com'
+app.config['MAIL_PASSWORD'] = '09juin1996'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello', sender = 'seckabdou632@gmail.com', recipients = ['abdouseck632@gmail.com'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
+
+if __name__ == '__main__':
+   app.run(debug = True)
